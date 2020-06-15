06ca87d080275d589823cd3ccc0e663c2a7de309
LasaBir <stanleymines@gmail.com>
2020-06-15 15:21:56 -0700

    Q functions, much logic on triangle float distribution, not sure if they work

--- ench.js
+++ ench.js
@@ -170,14 +170,63 @@ class Q
     var str = (this.n*p/this.d).toString()
     while (str.length<width-pow+1)
       str = "0"+str;
-    str = str.substring(0,str.length-(width-pow))+"."+str.substring(str.length-(width-pow));
+    if (str.length>pow)
+      str = str.substring(0,str.length-(width-pow))+"."+str.substring(str.length-(width-pow));
     return str;
   }
-  mu(o){return this.multiply(o);}
-  di(o){return this.divide(o);}
-  sub(o){return this.subtract(o);}
-  neg(){return this.negate();}
-  reci(){return this.reciprocal();}
+  
+  reci(){return this.reciprocal();}   //RECIprocal
+  neg(){return this.negate();}        //NEGate
+
+  rt(){return this.truncate();}       //RoundTruncate
+  r(){return this.round();}           //Round
+  ro(){return this.roundout();}       //RoundOut
+
+  lt(o){return this.lessthan(o);}     //LessThan
+  le(o){return this.lessequal(o);}    //LessthanorEqualto
+  gt(o){return this.greaterthan(o);}  //GreaterThan
+  ge(o){return this.greaterequal(o);} //GreaterthanorEqualto
+  e(o){return this.eq(o);}            //Equals
+  eq(o){return this.equals(o);}       //EQuals
+
+  mu(o){return this.multiply(o);}     //MUltiply
+  di(o){return this.divide(o);}       //DIvide
+  a(o){return this.add(o);}           //Add
+  s(o){return this.sub(o);}           //Subtract
+  sub(o){return this.subtract(o);}    //SUBtract
+  
+  reciprocal(){return new Q(this.d,this.n);}
+  negate(){return new Q(-this.n,this.d);}
+  truncate(){return new Q(this.n/this.d);}
+  round(){return this.multiply(2n).truncate().add(1n).divide(2n).truncate();}
+  roundout()
+    {if (this.n%this.d==0) return this;
+    return truncate().add(1n);}
+  lessthan(other) //TODO THESE FAIL WHEN DENOM IS NEGATIVE
+    {if (other instanceof Q) return this.n*other.d<other.n*this.d;
+    else throw 'Expected other instanceof Q';}
+  lessequal(other)
+    {if (other instanceof Q) return this.n*other.d<=other.n*this.d;
+    else throw 'Expected other instanceof Q';}
+  greaterthan(other)
+    {if (other instanceof Q) return this.n*other.d>other.n*this.d;
+    else throw 'Expected other instanceof Q';}
+  greaterequal(other)
+    {if (other instanceof Q) return this.n*other.d>=other.n*this.d;
+    else throw 'Expected other instanceof Q';}
+
+  equals(other)
+  {
+    if (other instanceof Q)
+      if (this.n*other.d==other.n*this.d)
+        if (this.n<other.n)
+          { other.n=this.n; other.d=this.d; return true; }
+        else if (this.n>other.n)
+          { this.n=other.n; this.d=other.d; return true; }
+        else return true;
+      else return false;
+    else throw 'Expected other instanceof Q';
+  }
   multiply(other)
   {
     if (typeof other == 'bigint')
@@ -185,9 +234,9 @@ class Q
     if (other instanceof Q)
     {
       if (other.n==0n)
-        return Q.ZERO;
+        return ZERO;
       if (this.n==other.d&&this.d==other.n)
-        return Q.ONE;
+        return ONE;
       if ((this.n%other.d==0&&other.d!=1n)||(other.n%this.d==0&&this.d!=1n)||(this.d%other.n==0&&other.n!=1n)||(other.d%this.n==0&&this.n!=1n))
       {
         if (this.n%other.d==0&&other.d!=1n)
@@ -224,15 +273,16 @@ class Q
       return new Q(this.n*other.d-other.n*this.d,this.d*other.d);
     throw 'Expected other instanceof Q'
   }
-  negate(){return new Q(-this.n,this.d);}
 }
-class QP
-{
-  constructor(a,b,c=null,d=null)
-  {
+// class QP
+// {
+//   constructor(a,b,c=null,d=null)
+//   {
 
-  }
-}
+//   }
+// }
+function q(a,b=null){return new Q(a,b);}
+// function qp(a,b,c=null,d=null){return new QP(a,b,c,d);}
 const ZERO = new Q(0n,1n);
 const ONE = new Q(1n,1n);
 const TWO = new Q(2n,1n);
@@ -295,46 +345,63 @@ class probdistrib
   get weights(){return this.wts;}
 
   // value *(scale±radius)+offset
-  // triangleFloatDistMultRounded(scale, radius, offset)
-  // {
-  //   var ret = new probdistrib(0,-1);
-  //   for (var i = 0; i<this.weights.length; i++)
-  //   {
-  //     var val = this.weights[i].value;
-  //     var b_a = val*(scale-radius)+offset;
-  //     var b_b = val*(scale+radius)+offset;
-  //     var denom = 1n;
-  //     for (l in ba:bb)
-  //     {
-  //       var lm=l-.5;
-  //       var lp=l+.5;
-  //       var x_aa=min();
-  //       var x_ab=;
-  //       var x_ba=;
-  //       var x_bb=;
+  triangleFloatDistMultRounded(scale, radius, offset = ZERO)
+  {
+    if (scale instanceof Q || typeof scale == 'bigint' || (typeof scale == 'number' && scale.toString()==BigInt(scale).toString()))
+      scale = new Q(scale);
+    if (radius instanceof Q || typeof radius == 'bigint' || (typeof radius == 'number' && radius.toString()==BigInt(radius).toString()))
+      radius = new Q(scale);
+    if (offset instanceof Q || typeof offset == 'bigint' || (typeof offset == 'number' && offset.toString()==BigInt(offset).toString()))
+      offset = new Q(offset);
+    var ret = new probdistrib(0,-1);
+    for (var i = 0; i<this.weights.length; i++)
+    {
+      var val = new Q(this.weights[i].value);
 
-  //       integral from newval-.5 to newval+.5 of triangle as Q.
-  //       // a:
-  //       //   ((l+.5)^2-(v-sr)^2)/2r+(1-sv/r)((l+.5)-(v-sr))
-  //       // b:
-  //       //   (l-vs)/r+1;
-  //       // c:
-  //       //   (8svl-4(vs)^2-4l^2-1)/4r+1
-  //       // d:
-  //       //   (vs-l)/r+1;
-  //       // e:
-  //       //   ((l-.5)^2-(v+sr)^2)/2r+(1+sv/r)((v+sr)-(l-.5))
-  //       // ac:
-  //       //   nah
-  //       // ce:
-  //       //   nah
-  //     }
-  //   }
+      var vs = val.m(scale);
+      var vr2 = val.m(radius).m(2n);
+      var sdr = scale.di(radius);
 
-  //   for()
-  //     ret=ret.union(each(val));
-  //   return ret;
-  // }
+      var b_a = val.mu(scale.s(radius)).a(offset);
+      var b_af = b_a.floor();
+      var b_b = val.mu(scale.a(radius)).a(offset);
+      var b_bc = b_a.ceiling();
+      var denom = 1n;
+      for (var l=b_af;l.le(b_bc);l.a(1n))
+      {
+        var lm=l.s(q(1,2)).s(offset);
+        var lp=l.a(q(1,2)).s(offset);
+        var x_aa=min(max(b_a,lm),vs);
+        var x_ab=min(max(b_a,lp),vs);
+        var x_ba=min(max(vs,lm)b_b);
+        var x_bb=min(max(vs,lp)b_b);
+        var x_ga=x_ab.m(x_ab).s(x_aa.m(x_aa)).di(vr2).a(ONE.s(sdr).mu(x_ab.s(x_aa)));
+        var x_gb=x_ba.m(x_ba).s(x_bb.m(x_bb)).di(vr2).a(ONE.a(sdr).mu(x_bb.s(x_ba)));
+        var x=x_ga+x_gb;
+        console.log(val.toString()+" "+l.toString()+" "+x.toString());
+
+        // integral from newval-.5 to newval+.5 of triangle as Q.
+        // a:
+        //   ((l+.5)^2-(v-sr)^2)/2r+(1-sv/r)((l+.5)-(v-sr))
+        // b:
+        //   (l-vs)/r+1;
+        // c:
+        //   (8svl-4(vs)^2-4l^2-1)/4r+1
+        // d:
+        //   (vs-l)/r+1;
+        // e:
+        //   ((l-.5)^2-(v+sr)^2)/2r+(1+sv/r)((v+sr)-(l-.5))
+        // ac:
+        //   nah
+        // ce:
+        //   nah
+      }
+    }
+
+    for()
+      ret=ret.union(each(val));
+    return ret;
+  }
 
   simplify()
   {
@@ -476,6 +543,19 @@ function main()
   console.log(val.toDecimal());
   console.log(val2.toDecimal());
   console.log(val3.toDecimal());
+  pi = new Q(262452630335382199398n,83541266890691994833n);
+  console.log(pi.toString(150));
+  console.log(pi.toDecimal(150));
+  pir = new Q(262452630335382199398n,83541266890n);
+  console.log(pir.toDecimal(17));
+  console.log(pir.toDecimal(9));
+  console.log(pir.toDecimal(10));
+  console.log(pir.toDecimal(11));
+  console.log(pir.toDecimal(12));
+  r13o83s = new Q(350629275419n,669935023473116n);
+  console.log(r13o83s.toDecimal(5));
+  r13o83s = new Q(350629275419n,669935023473116n*669935023473116n);
+  console.log(r13o83s.toDecimal(5));
 }
 main();
 
